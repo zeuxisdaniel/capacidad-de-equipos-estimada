28df1356f29f2510b3ff37e3b2ef0c911abf4cef
Zeuxis Daniel <zeuxisdaniel.villegasa@bbva.com>
2019-03-19 16:29:42 -0600

    Se añade código del sistema experto al servicio obtener-recursos en flask

--- services/views.py
+++ services/views.py
@@ -1,5 +1,8 @@
 from services import app
 from flask import Flask, render_template, request, redirect, url_for, Response
+from .rules.project_recomendation_rules import ProjectConditions
+from .rules.project_recomendation_rules import TimeProjectConditions
+from .rules.project_recomendation_rules import ProjectRecomendationRules
 import json
 
 @app.route('/services/health-check', methods=['GET'])
@@ -7,4 +10,30 @@ def health_check():
   body = {
       'msg': 'Hola Mundo!',
   }
+  return Response(json.dumps(body), mimetype='application/json')
+
+@app.route('/services/obtener-recursos', methods=['POST'])
+def obtener_resursos():
+  contenido = request.get_json()
+
+  engine = ProjectRecomendationRules()
+  engine.reset()
+
+
+  #Probamos con el caso de prueba 1
+  engine.declare(
+    ProjectConditions(contenido['tecnologia1']),
+    ProjectConditions(contenido['tecnologia2']),
+    ProjectConditions(contenido['tecnologia3']),
+    ProjectConditions(contenido['tecnologia4']),
+    TimeProjectConditions(timeProject=contenido['timeProject'])
+  )
+
+  engine.run()
+  message, image = engine.get_recomendation()
+  body = {
+      'message': message,
+      'image': image
+  }
+
   return Response(json.dumps(body), mimetype='application/json')
