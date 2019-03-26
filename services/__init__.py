3aa56bcaf369bbd49208993056db9a22b2b62444
Zeuxis Daniel <zeuxisdaniel.villegasa@bbva.com>
2019-03-26 09:52:38 -0600

    Se añade CORS a flask para aceptar peticiones desde el front

--- services/__init__.py
+++ services/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 
 BUILD_DIR = 'build'
 build_path = '../{}'.format(BUILD_DIR)
 
 app = Flask(__name__, static_folder=build_path)
+CORS(app)
 
 import services.static
 import services.views
